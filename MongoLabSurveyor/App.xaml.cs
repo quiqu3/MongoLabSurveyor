f392b84db3976c912f40ae065e58b788ce292745
acasquete <acasquete@outlook.com>
2013-09-30 23:57:59 +0200

    Basic MVVM Implementation

--- MongoLabSurveyor/App.xaml.cs
+++ MongoLabSurveyor/App.xaml.cs
@@ -13,24 +13,6 @@ namespace MongoLabSurveyor
 {
     public partial class App : Application
     {
-        private static MainViewModel viewModel = null;
-
-        /// <summary>
-        /// A static ViewModel used by the views to bind against.
-        /// </summary>
-        /// <returns>The MainViewModel object.</returns>
-        public static MainViewModel ViewModel
-        {
-            get
-            {
-                // Delay creation of the view model until necessary
-                if (viewModel == null)
-                    viewModel = new MainViewModel();
-
-                return viewModel;
-            }
-        }
-
         /// <summary>
         /// Provides easy access to the root frame of the Phone Application.
         /// </summary>
@@ -81,15 +63,9 @@ namespace MongoLabSurveyor
         {
         }
 
-        // Code to execute when the application is activated (brought to foreground)
-        // This code will not execute when the application is first launched
         private void Application_Activated(object sender, ActivatedEventArgs e)
         {
-            // Ensure that application state is restored appropriately
-            if (!App.ViewModel.IsDataLoaded)
-            {
-                App.ViewModel.LoadData();
-            }
+
         }
 
         // Code to execute when the application is deactivated (sent to background)
---
+++ MongoLabSurveyor/View/DatabasePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MongoLabSurveyor.ViewModels;
+
+namespace MongoLabSurveyor.View
+{
+    public partial class DatabasesPage : PhoneApplicationPage
+    {
+        public DatabasesPage()
+        {
+            InitializeComponent();
+            DatabaseViewModel productViewModel = new DatabaseViewModel();
+
+            productViewModel.GetDatabases();
+
+            this.DataContext = productViewModel;
+        }
+    }
+}
