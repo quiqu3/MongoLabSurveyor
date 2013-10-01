2094aaed9d9a907690e849c136f836270861379f
acasquete <acasquete@outlook.com>
2013-10-01 13:16:05 +0200

    Added model and viewmodel

---
+++ MongoLabSurveyor/Model/MongoLabDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoLabSurveyor.Model
+{
+    public class MongoLabDB
+    {
+        public string Name { get; set; }
+
+        public List<Collection> Collections { get; set; }
+    }
+}
---
+++ MongoLabSurveyor/ViewModel/DatabaseViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using MongoLabSurveyor.Resources;
+using MongoLabSurveyor.Model;
+using System.IO.IsolatedStorage;
+using MongoLabSurveyor.Service;
+
+namespace MongoLabSurveyor.ViewModel
+{
+    public class DatabaseViewModel : ViewModelBase
+    {
+        private ObservableCollection<MongoLabDB> databases;
+        public ObservableCollection<MongoLabDB> Databases
+        {
+            get
+            {
+                return databases;
+            }
+            set
+            {
+                databases = value;
+                RaisePropertyChanged("Databases");
+            }
+        }
+
+        public void GetDatabases()
+        {
+            GetDefaultDatabases();
+        }
+
+        public async void GetDefaultDatabases()
+        {
+            var dbs = new ObservableCollection<MongoLabDB>();
+            var service = new MongoLabDataService();
+            var databases = await service.GetDatabases();
+
+            databases.ToList().ForEach(async dbname => dbs.Add(new MongoLabDB() { Name = dbname, Collections = await service.GetCollections(dbname) }));
+
+            Databases = dbs;
+        }
+
+        
+    }
+}
