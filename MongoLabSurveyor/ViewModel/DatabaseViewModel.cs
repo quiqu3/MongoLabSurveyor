1e7e6fe2ef1da5a796398676081a192e5ae446f3
acasquete <acasquete@outlook.com>
2013-10-02 20:49:05 +0200

    Added error message in databasepage

--- MongoLabSurveyor/ViewModel/DatabaseViewModel.cs
+++ MongoLabSurveyor/ViewModel/DatabaseViewModel.cs
@@ -9,18 +9,21 @@
 
     public class DatabaseViewModel : ViewModel
     {
+        private const string ERROR_INVALIDAPIKEY = "error loading data. please ensure that your device is connected to the network and the api key is correct.";
+        private const string ERROR_MISSINGAPIKEY = "The Api Key is not configured. Please enter settings and enter the valid api key.";
+
         private readonly IMongoLabDataService mongoLabDataService;
         private readonly ISettingsStore settingsStore;
 
         public DatabaseViewModel(ISettingsStore settingsStore, IMongoLabDataService mongoLabDataService, INavigationService navigationService)
-            : base (navigationService)
-        {            
+            : base(navigationService)
+        {
             this.mongoLabDataService = mongoLabDataService;
             this.settingsStore = settingsStore;
 
             Refresh();
         }
-       
+
         private ObservableCollection<MongoLabDB> _databases;
         public ObservableCollection<MongoLabDB> Databases
         {
@@ -34,7 +37,7 @@
                 RaisePropertyChanged("Databases");
             }
         }
-
+        
         private bool isLoading;
         public bool IsLoading
         {
@@ -49,29 +52,55 @@
             }
         }
 
+        private string errorMessage;
+        public string ErrorMessage
+        {
+            get
+            {
+                return errorMessage;
+            }
+            set
+            {
+                errorMessage = value;
+                RaisePropertyChanged("ErrorMessage");
+            }
+        }
+
         public void Refresh()
         {
-            GetDefaultDatabases();
+            if (settingsStore.ApiKey != String.Empty)
+            {
+                GetDefaultDatabases();
+            }
+            else
+            {
+                ErrorMessage = ERROR_MISSINGAPIKEY;
+            }
         }
 
         private async void GetDefaultDatabases()
         {
-            if (settingsStore.ApiKey != String.Empty)
+            var dbs = new ObservableCollection<MongoLabDB>();
+
+            try
             {
                 IsLoading = true;
-
-                var dbs = new ObservableCollection<MongoLabDB>();
 
                 var databases = await mongoLabDataService.GetDatabases();
 
                 databases.ToList().ForEach(async dbname => dbs.Add(new MongoLabDB() { Name = dbname, DbStats = await mongoLabDataService.GetDbStats(dbname) }));
-
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ERROR_INVALIDAPIKEY;
+            }
+            finally
+            {
                 Databases = dbs;
-
                 IsLoading = false;
             }
         }
 
-        
+
     }
 }
